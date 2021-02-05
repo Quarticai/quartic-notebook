8e7fb16a8f22922462943c075132ec0137d7dfe0
Sourcery AI <>
2021-02-05 12:06:27 +0000

    'Refactored by Sourcery'

--- notebook/db_util.py
+++ notebook/db_util.py
@@ -137,11 +137,8 @@ class ExecuteQueries:
         Get address for all the mlnodes
         :return: List of address for Mlnodes instances.
         """
-        _addr = []
         mlnodes = MLNode.objects.all()
-        for mlnode in mlnodes:
-            _addr.append(f'http://{str(mlnode.ip_address)}:{mlnode.port}')
-        return _addr
+        return [f'http://{str(mlnode.ip_address)}:{mlnode.port}' for mlnode in mlnodes]
 
     def get_mlnode_address_with_field(self, column_name, column_value):
         """
