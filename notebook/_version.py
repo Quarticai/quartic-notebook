52f4f73ab518120fbf96c10e8303d6cce0df9aec
mprasoonquartic <46556770+mprasoonquartic@users.noreply.github.com>
2021-03-17 14:31:20 +0530

    Update _version.py

--- notebook/_version.py
+++ notebook/_version.py
@@ -9,4 +9,4 @@ store the current version info of the notebook.
 
 # Next beta/alpha/rc release: The version number for beta is X.Y.ZbN **without dots**.
 
-__version__ = 'rc1.1.0.0'
+__version__ = 'rc2.1.0.0'
